0f7bcbad35ecde57bdff2f82572a0dbd7ae4d2ba
Ismoil98 <ismoil6366>
2024-06-14 19:17:35 +0500

    added

--- src/routes/Locations.jsx
+++ src/routes/Locations.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Locations = () => {
   return (
-    <div className='text-7xl h-full bg-main'>
-        Ismoil AKA
+    <div className='text-7xl h-full bg-main p-5'>
+      <div className='flex justify-between align-items-center'>
+        <h1 className='text-6xl text-'>Qo'shish</h1>
+        <div className='bg-blue-500 text-white rounded hover:bg-blue-700'>
+          <div className='border-solid border-2 border-white m-2 pb-2 px-3 mx-4 text-4xl rounded-full'>
+            +
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
