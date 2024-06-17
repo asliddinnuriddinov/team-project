69841c843b2ecdf6ee7dce442b0e7c4f757547cc
Steelgrou <steelgrou03@gmail.com>
2024-06-17 17:57:30 +0500

    fix delete button add modal

--- src/routes/Categories.jsx
+++ src/routes/Categories.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { LiaArchiveSolid, LiaEyeDropperSolid } from "react-icons/lia";
 import { ToastContainer, toast } from 'react-toastify';
 
 const Categories = () => {
-  const notify = () => toast();
+  const notify = (message) => toast(message);
   const [categories, setCategories] = useState([]);
   const [data, setData] = useState({ name_en: "", name_ru: "", image_src: null });
+  const [deleteOpen, setDeleteOpen] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
   const urlImg = "https://autoapi.dezinfeksiyatashkent.uz/api/uploads/images/";
   const token = localStorage.getItem('token');
 
@@ -18,7 +20,7 @@ const Categories = () => {
       .catch(error => {
         console.error("Error fetching categories:", error);
       });
-  }
+  };
 
   const deleteCategory = (id) => {
     fetch(`https://autoapi.dezinfeksiyatashkent.uz/api/categories/${id}`, {
@@ -29,13 +31,13 @@ const Categories = () => {
     })
       .then(res => res.json())
       .then(() => {
-        toast("File deleted");
+        notify("Category deleted");
         getCategory();
       })
       .catch(error => {
         console.error("Error deleting category:", error);
       });
-  }
+  };
 
   const editCategory = (item) => {
     const formData = new FormData();
@@ -54,13 +56,13 @@ const Categories = () => {
     })
       .then(res => res.json())
       .then(() => {
-        toast("Category edited");
+        notify("Category edited");
         getCategory();
       })
       .catch(error => {
         console.error("Error editing category:", error);
       });
-  }
+  };
 
   useEffect(() => {
     getCategory();
@@ -84,60 +86,77 @@ const Categories = () => {
     })
       .then(res => res.json())
       .then(() => {
-        toast("Category created");
+        notify("Category created");
         setData({ name_en: "", name_ru: "", image_src: null });
         getCategory();
       })
       .catch(err => {
         console.error("Error creating category:", err);
       });
-  }
+  };
 
   return (
-    
     <div className='text-7xl h-full bg-main overflow-y-scroll px-14 py-10'>
       <div className="container bg-white rounded-xl py-5">
-        <table >
-          <tr className='flex md:items-center gap-8  rounded px-10 mr-7' style={{ background: '#E3EFFE' }}>
-            <th className='text-base w-[120px] pt-5 pb-5'>№</th>
-            <th className='text-base w-[240px]'>Name</th>
-            <th className='text-base w-[270px]'>Text</th>
-            <th className='text-base w-[270px]'>images</th>
-            <th className='text-base w-[50px]'>Actions</th>
-          </tr>
-          {categories && categories.map((item, index) => (
-            <tr className='flex md:items-center gap-8 ' key={index}>
-              <td className='w-[200px] rounded bg-slate-50 p-2'><h3 className='text-sm'>{index + 1 + '.'} {item.name_en}</h3></td>
-              <td className='text-sm w-[250px] bg-slate-50 p-2'>{item.name_en}</td>
-              <td className='text-sm w-[250px] bg-slate-100 p-2'>{item.name_ru}</td>
-              <td><img className='w-[200px] h-26 mt-10 rounded-lg' src={`${urlImg}${item.image_src}`} alt={item.name_ru} /></td>
-              <td className='flex gap-5 w-[200px]'>
-                <button className='text-sm bg-blue-600 pt-3 pl-4 pr-4 pb-3 rounded-md text-white' onClick={() => editCategory(item)}>Edit</button>
-                <button className='text-sm bg-red-600 pt-3 pl-4 pr-4 pb-3 rounded-md text-white'  onClick={() => deleteCategory(item.id)}>Del</button>
-              </td>
+        <table>
+          <thead>
+            <tr className='flex md:items-center gap-8 rounded px-10 mr-7' style={{ background: '#E3EFFE' }}>
+              <th className='text-base w-[120px] pt-5 pb-5'>№</th>
+              <th className='text-base w-[240px]'>Name</th>
+              <th className='text-base w-[270px]'>Text</th>
+              <th className='text-base w-[270px]'>Images</th>
+              <th className='text-base w-[50px]'>Actions</th>
             </tr>
-          ))}
+          </thead>
+          <tbody>
+            {categories && categories.map((item, index) => (
+              <tr className='flex md:items-center gap-8' key={item.id}>
+                <td className='w-[200px] rounded bg-slate-50 p-2'>
+                  <h3 className='text-sm'>{index + 1}. {item.name_en}</h3>
+                </td>
+                <td className='text-sm w-[250px] bg-slate-100 p-2'>{item.name_ru}</td>
+                <td><img className='w-[200px] h-26 mt-10 rounded-lg' src={`${urlImg}${item.image_src}`} alt={item.name_ru} /></td>
+                <td className='flex gap-5 w-[200px]'>
+                  <button className='text-sm bg-blue-600 pt-3 pl-4 pr-4 pb-3 rounded-md text-white' onClick={() => editCategory(item)}>Edit</button>
+                  <button className='text-sm bg-red-600 pt-3 pl-4 pr-4 pb-3 rounded-md text-white' onClick={() => { setSelectedCategory(item); setDeleteOpen(true); }}>Del</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
 
         <form onSubmit={createCategory} className='mt-10 flex-col items-center'>
           <div className='flex flex-col'>
-            <label className='text-sm' htmlFor="">Create and Edit</label>
+            <label className='text-sm'>Create and Edit (English)</label>
             <input className='text-sm border-current border-2 p-2' type="text" placeholder="add or edit text" value={data.name_en} onChange={(e) => setData({ ...data, name_en: e.target.value })} />
           </div>
           <div className='flex flex-col'>
-            <label className='text-sm' htmlFor="">Create and Edit</label>
+            <label className='text-sm'>Create and Edit (Russian)</label>
             <input className='text-sm border-current border-2 p-2' type="text" placeholder="add or edit text" value={data.name_ru} onChange={(e) => setData({ ...data, name_ru: e.target.value })} />
           </div>
           <div className='flex flex-row items-center'>
-            <label htmlFor=""></label>
-            <input className='text-sm' type="file" onChange={(e) => setData({ ...data, image_src: e.target.files[0] })} />
-            <button className='text-sm border-current border-2 p-2' type='submit ' onClick={notify}>Create</button>
-            <ToastContainer />
+            <label htmlFor="fileInput" className='text-sm mr-2'>Image:</label>
+            <input className='text-sm' id="fileInput" type="file" onChange={(e) => setData({ ...data, image_src: e.target.files[0] })} />
+            <button className='text-sm border-current border-2 p-2 ml-2' type='submit' onClick={notify}>Create</button>
           </div>
         </form>
       </div>
+
+      {deleteOpen && (
+        <div className="fixed top-0 left-0 w-full h-full bg-[#00000048] flex items-center justify-center z-50">
+          <div className="w-[400px] overflow-y-scroll h-[20vh] px-5 py-8 bg-white">
+            <p className='text-xl pb-[15px] text-center'>Delete your file now?</p>
+            <div className='flex justify-around'>
+              <button className='border-2 rounded-sm border-gray-300 bg-gray-100 p-3' onClick={() => setDeleteOpen(false)}>No</button>
+              <button className='text-sm bg-red-600 pt-3 pl-4 pr-4 pb-3 rounded-md text-white' onClick={() => { deleteCategory(selectedCategory.id); setDeleteOpen(false); }}>Yes</button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      <ToastContainer />
     </div>
-  )
-}
+  );
+};
 
 export default Categories;
